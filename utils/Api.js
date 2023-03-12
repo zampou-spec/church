83575cc801ada12a5bea664518804e61bed39973
Zampou Ibrahim <zampou@Zampous-MacBook-Pro.local>
2023-03-12 11:46:33 +0000

    New achirtecture https

--- utils/Api.js
+++ utils/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const httpClient = axios.create({
-  baseURL: ` http://api-churche.webturba.com/api/`,
+  baseURL: ` https://api-churche.webturba.com/api/`, 
   headers: {
     'Content-Type': 'application/json',
   },
